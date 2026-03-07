cfe542a0bd176203a8350ac0b3d79618e0b37ef8
Prashant Singh <prshnnt@gmail.com>
2026-03-07 13:40:20 +0530

    fixed more animations till group9

--- test_bb.py
+++ test_bb.py
@@ -17,14 +17,16 @@ with open(JSON_PATH, "r") as f:
 sheet = pygame.image.load(IMAGE_PATH).convert_alpha()
 
 groups = list(data.keys())
-group_index = 4 # till g4 done !
+group_index = 10 # till g5 done !
 frame_index = 0
 frames = data[groups[group_index]]
 
 def get_frame(frame):
     rect = pygame.Rect(frame["x"], frame["y"], frame["w"], frame["h"])
     img = pygame.Surface((frame["w"], frame["h"]), pygame.SRCALPHA)
+    img.set_colorkey((0,64,128))
     img.blit(sheet, (0, 0), rect)
+    img = img.convert_alpha(img)
     img = pygame.transform.scale(img, (frame["w"]*SCALE, frame["h"]*SCALE))
     return img
 
@@ -58,7 +60,7 @@ while running:
     frame = get_frame(frames[frame_index])
 
     screen.fill((30, 30, 30))
-    rect = frame.get_rect(center=(200, 150))
+    rect = frame.get_rect()
     screen.blit(frame, rect)
 
     pygame.display.set_caption(f"Animation: {groups[group_index]}  Frame: {frame_index}")
