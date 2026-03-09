f0ab73e632d1d546e20ccd9a435ae61d681190b6
Prashant Singh <prshnnt@gmail.com>
2026-03-10 00:05:02 +0530

    changed name of groups in naruto.json

--- core/config.py
+++ core/config.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 import pygame
 WINDOW_SIZE = (400, 400)
 WINDOW_TITLE = "Naruto Game"
 FPS = 60
 ALLOWED_KEYS = [pygame.K_ESCAPE, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, pygame.K_SPACE, pygame.K_b , pygame.K_y]
-=======
-WINDOW_SIZE = (400, 400)
-WINDOW_TITLE = "Naruto Game"
->>>>>>> 0238a1b1ad401cc22b69d5b99ac9d82db3bd871d
+
+COLORKEY = (0, 64, 128)
