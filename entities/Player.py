6685a968846bab982014adf408d20d560864f9f8
Prashant Singh <prshnnt@gmail.com>
2026-03-21 02:09:57 +0530

    added invincible property such that player flicker when invincible , cahnged move function to move_x and move_y so that it is compatible with my collison function

--- entities/Player.py
+++ entities/Player.py
@@ -1,13 +1,12 @@
 import pygame as pg
 import json
+from Components import Ground
 from States.Base import BaseState
 from core.config import *
 from core.GameObject import GameObject
 from Animator.Base import Animator
 from States import FallState, IdleState, JumpState, RunState
 
-
-GRAVITY = 1      # pixels per second squared
 
 class Player(GameObject):
     def __init__(self, pos):
@@ -20,6 +19,7 @@ class Player(GameObject):
         self.in_air = True
 
         self.health: int = 100
+        self.invincible: bool = False
         self.alive: bool = True
         self.facing: int = 1           # 1 = right, -1 = left
         self.dt = 0
@@ -49,10 +49,43 @@ class Player(GameObject):
             self.facing = 1
         if self.alive and self.state is not None:
             self.state.handle_action(self,action)
-    def move(self):
-        self.position += self.velocity
+    def move_x(self):
+        self.position.x += self.velocity.x
         self.rect.x = self.position.x
+    def move_y(self):
+        self.position.y += self.velocity.y
         self.rect.y = self.position.y
+
+    def collide_wall(self,platforms:list[Ground]):
+        # --- X AXIS MOVEMENT ---
+        self.move_x()
+        # X Collision Check
+        for platform in platforms:
+            if self.rect.colliderect(platform.rect):
+                if self.velocity.x > 0:
+                    self.rect.right = platform.rect.left
+                elif self.velocity.x < 0:
+                    self.rect.left = platform.rect.right
+                
+                # CRITICAL FIX: Sync self.x with the new rect position
+                self.position.x = self.rect.x 
+
+        # --- Y AXIS MOVEMENT ---
+        self.in_air = True
+        self.move_y()
+        # Y Collision Check
+        for platform in platforms:
+            if self.rect.colliderect(platform.rect):
+                if self.velocity.y > 0:
+                    self.rect.bottom = platform.rect.top
+                    self.velocity.y = 0
+                    self.in_air = False
+                elif self.velocity.y < 0:
+                    self.rect.top = platform.rect.bottom
+                    self.velocity.y = 0
+                
+                # CRITICAL FIX: Sync self.y with the new rect position
+                self.position.y = self.rect.y
 
     def update(self,scene , dt: float):
         """Update state logic, move the rect, resolve collisions."""
@@ -60,23 +93,14 @@ class Player(GameObject):
 
         self.state.update(self,dt)
 
-        self.move()
-
-        # THEN check collision
-        if self.rect.bottom >= SCREEN_HEIGHT - GROUND_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT - GROUND_HEIGHT
-            if self.in_air:
-                self.change_state("idle")
-            self.in_air = False
-            self.velocity.y = 0
-        else:
-            if not self.in_air:
-                self.change_state("fall")
-            self.in_air = True
+        self.collide_wall(scene.layers["world"])
 
         self.animator.update(dt)
         
     def draw(self,scene):
         """Delegate drawing to animator"""
-        pg.draw.rect(scene.screen,RED,self.rect)
+        if self.invincible:
+            if (pg.time.get_ticks() // 100) % 2 == 0:
+                return
+        # pg.draw.rect(scene.screen,RED,scene.camera.apply(self.rect))
         self.animator.draw(scene)
