8435ba40481f635ccd2f417e2bc295e66e8760ba
Prashant Singh <prshnnt@gmail.com>
2026-03-21 02:11:54 +0530

    modified the Ground class such that it accepts the position , width , height of ground tile , can be used further for proper tiles

--- Components/Ground.py
+++ Components/Ground.py
@@ -3,12 +3,12 @@ import pygame as pg
 from core.config import *
 
 class Ground(pg.sprite.Sprite,GameObject):
-    def __init__(self,image=None):
+    def __init__(self,width,height,pos=(0,SCREEN_HEIGHT-GROUND_HEIGHT),image=None):
         super().__init__()
         if image is None:
-            self.image = pg.Surface((SCREEN_WIDTH,GROUND_HEIGHT))
+            self.image = pg.Surface((width,height))
             self.image.fill(GREEN)
-            self.rect = self.image.get_rect(bottomleft=(0,SCREEN_HEIGHT))
+            self.rect = self.image.get_rect(topleft = pos)
         else:
             self.image = image
             self.rect = self.image.get_rect(bottomleft=(0,SCREEN_HEIGHT))
@@ -16,5 +16,5 @@ class Ground(pg.sprite.Sprite,GameObject):
     def update(self,game):
         pass
 
-    def draw(self,game):
-        game.screen.blit(self.image,self.rect)
+    def draw(self,scene):
+        scene.screen.blit(self.image,scene.camera.apply(self.rect))
